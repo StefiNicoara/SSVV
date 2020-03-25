fef0785ed225f0505520c46f9970e1c83248be5b
dianamnt13 <munteanu.diana0@gmail.com>
2020-03-25 10:48:46 +0200

    changed the root folder of the project

---
+++ src/test/java/MaxPointsParticipantsMV/AppTest.java
@@ -0,0 +1,37 @@
+package MaxPointsParticipantsMV;
+
+import MaxPointsParticipantsMV.Domain.Student;
+import MaxPointsParticipantsMV.Repository.StudentRepo;
+import MaxPointsParticipantsMV.Validator.StudentValidator;
+import org.junit.After;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    @Test
+    public void testAddStudentSuccess() {
+        StudentRepo studentRepo = new StudentRepo(new StudentValidator(),"C:\\Users\\Diana\\Facultate\\Year3\\SSVV\\Lab2\\Maven project\\SSVV\\src\\main\\java\\MaxPointsParticipantsMV\\studenti.xml");
+        Student student = new Student("6","Morea Razvan", 935, "mrie2365@scs.ubbcluj.ro", "Nicoara Stefania");
+        assertNull(studentRepo.save(student));
+        studentRepo.delete("6");
+    }
+
+    @Test
+    public void testAddStudentFailure() {
+        StudentRepo studentRepo = new StudentRepo(new StudentValidator(),"C:\\Users\\Diana\\Facultate\\Year3\\SSVV\\Lab2\\Maven project\\SSVV\\src\\main\\java\\MaxPointsParticipantsMV\\studenti.xml");
+        Student student = new Student("5","Munteanu Diana", 935, "maie2367@scs.ubbcluj.ro", "Nicoara Stefania");
+        assertEquals(student.getID(), studentRepo.save(student).getID());
+        assertEquals(student.getNume(), studentRepo.save(student).getNume());
+        assertEquals(student.getGrupa(), studentRepo.save(student).getGrupa());
+        assertEquals(student.getMail(), studentRepo.save(student).getMail());
+        assertEquals(student.getProfesor(), studentRepo.save(student).getProfesor());
+    }
+}
